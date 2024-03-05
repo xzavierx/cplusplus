25fbad4142e3ac5e1e05f8ef1a41aeff390453fa
yanxx2018 <abc@1.com>
2024-03-05 21:45:46 +0800

    增加日志

--- concurrent/async/async.cpp
+++ concurrent/async/async.cpp
@@ -7,6 +7,7 @@
 #include <setjmp.h>
 #include <stdexcept>
 #include <thread>
+// std::promise类似于golang的channel
 void setValue(std::promise<int> p) {
   // try{
   //   throw std::runtime_error("Ooooops!");
@@ -15,10 +16,13 @@ void setValue(std::promise<int> p) {
   //   return ;
   // }
   std::cout << "before setting value" << std::endl;
+  // 设置promise的值，设置完成以后，主线程立即都能获取到值了
   p.set_value(100);
   std::this_thread::sleep_for(std::chrono::seconds(1));
   std::cout << "end setting value" << std::endl;
 }
+// 子线程抛出异常可以调用promise::set_exception将异常传递出去，
+// 其他调用promise::get()的线程需要try-catch捕获这些异常
 
 void Promise() {
   std::promise<int> p;
@@ -26,11 +30,13 @@ void Promise() {
   std::thread t(setValue, std::move(p));
   std::cout << "Waiting for the thread to set value..." << std::endl;
   std::cout << "Value set by the thread: " << f.get() << std::endl;
-  t.detach();
+  t.join();
 }
 
 void SharedPromise() {
   std::promise<int> promise;
+  // 多个线程等待同一个异步操作的结果,
+  // 此时shared_future::get调用多次都不会出现问题，应该不会消耗内部状态了
   std::shared_future<int> future = promise.get_future();
   std::thread thread1(setValue, std::move(promise));
 
---
+++ docs/async.md
@@ -0,0 +1,18 @@
+# async用法
+std::async返回一个std::future
+```cpp
+  std::string fetchDataFromDB(std::string query) {}
+  std::future<std::string> resultFromDB = std::async(std::launch::async, fetchDataFromDB, "Data");
+  // 从future对象中获取数据,阻塞async的子线程结束
+  std::string dbData = resultFromDB.get();
+```
+
+# async启动策略
+1. std::launch::async: 再调用std::async后就开始执行
+2. std::launch::deferred: 这种策略意味着将在调用std::future::get()或std::future::wait()函数时延迟执行，换句话说，任务将在需要结果时同步执行
+3. std::launch::async | std::launch::deferred: 任务可在一个单独线程上异步执行，也可以延迟执行，具体取决于实现
+std::async函数的默认策略为第三种，需要注意的时是，不同编译器和操作系统会有不同的默认行为
+
+# std::future::get() vs. std::future::wait()
+std::future::get()是一个阻塞调用给，用于获取std::future对象表示的值或异常，如果异步任务没有完成，get()会阻塞，get()只能调用一次，因为它会移动或消耗掉std::future对象的状态。
+std::future::wait()也是一个阻塞调用，但它与get()的主要区别在于wait()不会返回任务的结果，它只是等待异步任务完成。如果任务已经完成，wait()会立即返回，如果任务没有完成wait()会阻塞当前线程，直到任务完成。
